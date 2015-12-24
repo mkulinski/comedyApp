ede77e97d832c92d084a70db4c1d52696822ba14
Michael Kulinski <me@michaelkulinski.com>
2015-12-24 23:32:36 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ComedyApp::Application.config.secret_key_base = '2fdb8131834028c4a683d19e572ac47476a0f796d971df9b43874b85a662f6cedca3d53399a854b7cf9bd00fe0f033b9ccaeb3f29111a8331902c1f8f7e5081b'
